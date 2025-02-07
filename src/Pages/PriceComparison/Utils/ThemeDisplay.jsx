ff43fa8073523dca313f081ddfb3c067a6797b8c
AndyKach <akwest6@gmail.com>
2025-02-08 00:14:23 +0100

    Search update

--- src/Pages/PriceComparison/Utils/ThemeDisplay.jsx
+++
@@ -1,9 +0,0 @@
-export const getThemeDisplay = (setData) => {
-  const theme = setData?.theme;
-  const themeGroup = setData?.themeGroup;
-  const categoryName = setData?.extendedData?.cz_category_name;
-
-  if (!theme && !categoryName) return null;
-  const displayTheme = theme || categoryName;
-  return themeGroup ? `${displayTheme}, ${themeGroup}` : displayTheme;
-};
--- src/Pages/PriceComparison/Utils/formatShopPrice.jsx
+++
@@ -1,9 +0,0 @@
-export const formatShopPrice = (price) => {
-  if (!price) return "N/A";
-
-  price = price
-    .replace(/\s/g, "")
-    .replace(",", ".")
-    .replace(/[^\d.]/g, ""); // Remove spaces, fix decimals
-  return price !== "" ? `${Math.floor(parseFloat(price))} Kč` : "N/A"; // Convert to integer and add "Kč"
-};
